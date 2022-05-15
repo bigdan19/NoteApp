29e092ee09cd7d2a52c3b4451a6fc098b9d67526
Daniel <daniel@Daniels-Mac-Pro.local>
2022-05-16 00:06:08 +0100

    Added different categorys, added category button , also new note button

--- NoteApp/MainTableViewController.swift
+++ NoteApp/MainTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
 
+    // OUR database for this current stage
     var datasource: [(sectionName: String, sectionElements: [String])] =
     [
         ("Work", ["Brush floor", "finish courses", "check rota"]),
@@ -18,28 +19,73 @@ class MainTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonPressed))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(organizeButtonPressed))
+        
     }
     
-    @objc func editButtonPressed() {
+    // Creating new Category for notes
+    @objc func organizeButtonPressed() {
+        let ac = UIAlertController(title: "Add Category", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         
+        let submitAction = UIAlertAction(title: "Done", style: .default) { [unowned ac] _ in
+            guard let name = ac.textFields![0].text else { return }
+            self.datasource.append(("\(name)", []))
+            self.tableView.reloadData()
+        }
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    // Creating new note in choosen category
+    @objc func addButtonPressed() {
+        let ac = UIAlertController(title: "Choose Category", message: nil, preferredStyle: .actionSheet)
+        for i in 0..<datasource.count {
+            ac.addAction(UIAlertAction(title: "\(datasource[i].0)", style: .default, handler: chooseCategory))
+        }
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    // function that is handling choosen category when creating new note
+    func chooseCategory(action: UIAlertAction) {
+        let ac = UIAlertController(title: "Write your note", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Done", style: .default) { [unowned self] _ in
+            guard let text = ac.textFields![0].text else { return }
+            for i in 0..<datasource.count {
+                if datasource[i].0 == action.title! {
+                    datasource[i].1.append(text)
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
 
+    // Setting number of sections in tableView
     override func numberOfSections(in tableView: UITableView) -> Int {
         return datasource.count
     }
 
+    // Setting number of rows in each section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datasource[section].sectionElements.count
     }
     
+    // setting title for every section
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return datasource[section].sectionName
     }
 
-    
+    // creating cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let note = datasource[indexPath.section].sectionElements[indexPath.row]
