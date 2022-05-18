9ab24b1cdb167f64b3100c114986df58ca32a009
Daniel <daniel@Daniels-Mac-Pro.local>
2022-05-18 21:56:39 +0100

    Added all empty fields error's

--- NoteApp/MainTableViewController.swift
+++ NoteApp/MainTableViewController.swift
@@ -16,6 +16,7 @@ class MainTableViewController: UITableViewController {
         ("Home", ["mop floor", "make laudry", "wash dishes"])
     ]
     
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,22 +27,34 @@ class MainTableViewController: UITableViewController {
         
     }
     
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    
     // Creating new Category for notes
     @objc func organizeButtonPressed() {
         let ac = UIAlertController(title: "Add Category", message: nil, preferredStyle: .alert)
         ac.addTextField()
-        
         let submitAction = UIAlertAction(title: "Create", style: .default) { [unowned ac] _ in
             guard let name = ac.textFields![0].text else { return }
-            self.datasource.append(("\(name)", []))
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
+            if name == "" {
+                self.showErrorMessage(title: "Error", message: "Category name can't be empty")
+            } else {
+                self.datasource.append(("\(name)", []))
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
+            
         }
         ac.addAction(submitAction)
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
     }
+    
     
     // Creating new note in choosen category
     @objc func addButtonPressed() {
@@ -54,21 +67,27 @@ class MainTableViewController: UITableViewController {
         present(ac, animated: true)
     }
     
-    // function that is handling choosen category when creating new note
+    
+    // Function that is handling choosen category when creating new note
     func choosenCategory(action: UIAlertAction) {
         let ac = UIAlertController(title: "Write your note", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
         let submitAction = UIAlertAction(title: "Create", style: .default) { [unowned self] _ in
             guard let text = ac.textFields![0].text else { return }
-            for i in 0..<datasource.count {
-                if datasource[i].0 == action.title! {
-                    datasource[i].1.append(text)
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
+            if text == "" {
+                showErrorMessage(title: "Error", message: "New note can't be empty")
+            } else {
+                for i in 0..<datasource.count {
+                    if datasource[i].0 == action.title! {
+                        datasource[i].1.append(text)
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
                     }
                 }
             }
+            
         }
         ac.addAction(submitAction)
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
@@ -85,13 +104,13 @@ class MainTableViewController: UITableViewController {
         return datasource[section].sectionElements.count
     }
     
-    // setting title for every section
+    // Setting title for every section
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 //        return datasource[section].sectionName
         return "\(datasource[section].sectionName + " (\(datasource[section].1.count))")"
     }
 
-    // creating cell
+    // Creating cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let note = datasource[indexPath.section].sectionElements[indexPath.row]
@@ -107,12 +126,19 @@ class MainTableViewController: UITableViewController {
         if editingStyle == .delete {
             tableView.beginUpdates()
             
-            // deleting item from our database
+            // Deleting item from our database
             
             datasource[indexPath.section].1.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             
+            // Checking if there is no more notes in a category - remove category
+            if datasource[indexPath.section].1.count == 0 {
+                datasource.remove(at: indexPath.section)
+                tableView.deleteSections(IndexSet(integer: indexPath.section), with: .fade)
+            }
             tableView.endUpdates()
+            
+            // Reloading tableView data after 0.3 sec
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ){
                 self.tableView.reloadData()
             }
@@ -120,3 +146,7 @@ class MainTableViewController: UITableViewController {
     }
 
 }
+
+
+// TODO: Potencially deleting a category from database if amount of notes are = 0
+// Updating the UI
